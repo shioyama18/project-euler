fe37880a3fe35ad3acbdf2844b75a3ec9d3e6d33
Shion Yamashita <shioyama1118@gmail.com>
2019-07-22 11:10:32 +0900

    Optimized problem 3

--- src/solution/p003.rs
+++ src/solution/p003.rs
@@ -3,13 +3,21 @@
 
 use crate::util::prime::Prime;
 
-pub fn solve(n: usize) -> usize {
-    let p = Prime::new();
+pub fn solve(mut n: usize) -> usize {
+    let mut p = Prime::new();
+    let mut prime_factors = Vec::new();
 
-    p.take_while(|x| x * x < n)
-        .filter(|x| n % x == 0)
-        .max()
-        .unwrap_or(n)
+    while let Some(x) = p.next() {
+        if x * x > n {
+            prime_factors.push(n);
+            break;
+        } else if n % x == 0 {
+            n /= x;
+            prime_factors.push(x);
+        }
+    }
+
+    *prime_factors.iter().max().expect("This will not be empty")
 }
 
 #[cfg(test)]
