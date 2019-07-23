87fd0fc690d682aa5313e696ea759dd064e8799f
Shion Yamashita <shioyama1118@gmail.com>
2019-07-23 16:50:20 +0900

    Optimized prime module

--- src/util/prime.rs
+++ src/util/prime.rs
@@ -1,18 +1,36 @@
 pub struct Prime {
-    curr: usize,
+    index: usize,
     primes: Vec<usize>,
 }
 
 impl Prime {
     pub fn new() -> Self {
         Prime {
-            curr: 2,
-            primes: Vec::new(),
+            index: 0,
+            primes: vec![2, 3],
         }
     }
 
-    fn is_prime(&self) -> bool {
-        self.primes.iter().all(|x| self.curr % x != 0)
+    fn expand(&mut self) {
+        let mut curr = self.primes.last().unwrap() + 2;
+
+        loop {
+            let mut remainder = 0;
+
+            for prime in self.primes.iter() {
+                remainder = curr % prime;
+                if remainder == 0 || prime * prime > curr {
+                    break;
+                }
+            }
+
+            if remainder == 0 {
+                curr += 2;
+            } else {
+                self.primes.push(curr);
+                break;
+            }
+        }
     }
 }
 
@@ -20,12 +38,12 @@ impl Iterator for Prime {
     type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while !self.is_prime() {
-            self.curr += 1;
+        while self.index >= self.primes.len() {
+            self.expand();
         }
 
-        let n = self.curr;
-        self.primes.push(n);
+        let n = self.primes[self.index];
+        self.index += 1;
         Some(n)
     }
 }
