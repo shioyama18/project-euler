6af9b1c3e3a03119b9b14a67d7ea9a5e64bfab49
Shion Yamashita <shioyama1118@gmail.com>
2019-07-24 23:16:42 +0900

    Added prime_factor method

--- src/solution/p003.rs
+++ src/solution/p003.rs
@@ -3,21 +3,9 @@
 
 use crate::util::prime::Prime;
 
-pub fn solve(mut n: usize) -> usize {
-    let mut p = Prime::new();
-    let mut prime_factors = Vec::new();
-
-    while let Some(x) = p.next() {
-        if x * x > n {
-            prime_factors.push(n);
-            break;
-        } else if n % x == 0 {
-            n /= x;
-            prime_factors.push(x);
-        }
-    }
-
-    *prime_factors.iter().max().expect("This will not be empty")
+pub fn solve(n: usize) -> usize {
+    let prime_factors = Prime::prime_factors(n);
+    *prime_factors.iter().max().unwrap()
 }
 
 #[cfg(test)]
--- src/util/prime.rs
+++ src/util/prime.rs
@@ -32,6 +32,23 @@ impl Prime {
             }
         }
     }
+
+    pub fn prime_factors(mut n: usize) -> Vec<usize> {
+        let mut p = Prime::new();
+        let mut prime_factors = Vec::new();
+
+        while let Some(x) = p.next() {
+            if x * x > n {
+                prime_factors.push(n);
+                break;
+            } else if n % x == 0 {
+                n /= x;
+                prime_factors.push(x);
+            }
+        }
+
+        prime_factors
+    }
 }
 
 impl Iterator for Prime {
